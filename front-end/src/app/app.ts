a96523b1f73159a3048880a42f60a168be1ac9ae
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-01 14:20:23 +0800

    Fix the main layout and make the components standalone

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MainLayout } from './layout/main-layout/main-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [MainLayout],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('Front-end');
--- front-end/src/app/layout/main-layout/main-layout.ts
+++ front-end/src/app/layout/main-layout/main-layout.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Navbar } from './navbar/navbar';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  standalone: true,
+  imports: [Navbar, RouterOutlet, Footer],
   templateUrl: './main-layout.html',
-  styleUrl: './main-layout.css',
+  styleUrls: ['./main-layout.css'],
 })
 export class MainLayout {}
