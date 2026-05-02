14999b068dd7a4be096315bac4361d2e256a1a93
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-02 15:23:05 +0800

    Add category card to the home page

--- front-end/src/app/features/pages/home-page/home-page.ts
+++ front-end/src/app/features/pages/home-page/home-page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {HeroSectionComponent} from '../../components/hero-section-component/hero-section-component'
+import {CategoryCardComponent} from '../../../shared/components/category-card-component/category-card-component'
 import { RouterLink, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroSectionComponent, CommonModule,RouterLink],
+  imports: [HeroSectionComponent, CommonModule,RouterLink, CategoryCardComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
