15be2016cc103d3b2064ece48f2ed0db02744a72
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-03 23:52:00 +0800

    Fix the spacing and remove the animation

--- front-end/src/app/features/pages/home-page/home-page.ts
+++ front-end/src/app/features/pages/home-page/home-page.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
 import {HeroSectionComponent} from '../../components/hero-section-component/hero-section-component'
 import {CategoryCardComponent} from '../../../shared/components/category-card-component/category-card-component'
+import {CardComponent} from '../../../shared/components/card-component/card-component'
 import { RouterLink, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroSectionComponent, CommonModule,RouterLink, CategoryCardComponent],
+  imports: [HeroSectionComponent, CommonModule,RouterLink, CategoryCardComponent,CardComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
-constructor(public router: Router) {}
-
-  isRoute(route: string): boolean {
-    return this.router.url === route;
-  }
 }
