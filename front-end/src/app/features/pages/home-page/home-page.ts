05b6eec1ef88f6af60387554029b178f06e32062
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-04 23:40:28 +0800

    Fix the format of the document

--- front-end/src/app/features/pages/home-page/home-page.ts
+++ front-end/src/app/features/pages/home-page/home-page.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {HeroSectionComponent} from '../../components/hero-section-component/hero-section-component'
 import {CategoryCardComponent} from '../../../shared/components/category-card-component/category-card-component'
 import {CardComponent} from '../../../shared/components/card-component/card-component'
 import { RouterLink, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-home-page',
-  imports: [HeroSectionComponent, CommonModule,RouterLink, CategoryCardComponent,CardComponent],
+  imports: [HeroSectionComponent, CommonModule,RouterLink, CategoryCardComponent,CardComponent,],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
