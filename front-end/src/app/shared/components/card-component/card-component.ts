dd786d714e51b66ba3dae00a33d4446cabbd850d
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-04 23:40:54 +0800

    Add card ui

--- front-end/src/app/shared/components/card-component/card-component.ts
+++ front-end/src/app/shared/components/card-component/card-component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject ,OnInit} from '@angular/core';
+import {PropertyService} from '../../../core/services/property-service/property-service'
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-card-component',
@@ -6,4 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './card-component.html',
   styleUrl: './card-component.css',
 })
-export class CardComponent {}
+export class CardComponent implements OnInit {
+  // Inject the service
+  private propertyService = inject(PropertyService);
+
+  protected readonly properties = this.propertyService.allProperties; 
+
+  ngOnInit() :void {
+    this.propertyService.getAllProperty();
+  }
+}
