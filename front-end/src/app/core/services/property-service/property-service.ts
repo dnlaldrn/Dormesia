0bac3fbedcf549aec7cbdb90e2d1fb7bf13923bc
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-05 22:39:11 +0800

    Add price in the service

--- front-end/src/app/core/services/property-service/property-service.ts
+++ front-end/src/app/core/services/property-service/property-service.ts
@@ -6,6 +6,7 @@ export interface Property {
   readonly address: string;
   readonly rating: number;
   readonly img: string;
+  readonly price: number;
 }
 
 @Injectable({
@@ -16,16 +17,18 @@ export class PropertyService {
     {
       propertyName: 'House 1',
       category: 'House',
-      rating: 2,
+      rating: 5,
       img: './assets/img/home-img.png',
       address: 'Kumintang, Batangas City',
+      price: 2000,
     },
     {
       propertyName: 'House 1',
       category: 'Bed Space',
-      rating: 2,
+      rating: 3,
       img: './assets/img/home-img.png',
       address: 'Kumintang, Batangas City',
+      price: 2000,
     },
     {
       propertyName: 'House 1',
@@ -33,6 +36,31 @@ export class PropertyService {
       rating: 2,
       img: './assets/img/home-img.png',
       address: 'Kumintang, Batangas City',
+      price: 2000,
+    },
+    {
+      propertyName: 'House 1',
+      category: 'Apartment',
+      rating: 2,
+      img: './assets/img/home-img.png',
+      address: 'Kumintang, Batangas City',
+      price: 2000,
+    },
+    {
+      propertyName: 'House 1',
+      category: 'Apartment',
+      rating: 2,
+      img: './assets/img/home-img.png',
+      address: 'Kumintang, Batangas City',
+      price: 2000,
+    },
+    {
+      propertyName: 'House 1',
+      category: 'Apartment',
+      rating: 2,
+      img: './assets/img/home-img.png',
+      address: 'Kumintang, Batangas City',
+      price: 2000,
     },
   ];
 
