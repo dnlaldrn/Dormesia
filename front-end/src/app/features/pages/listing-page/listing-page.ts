737a93968ca688cf6086577e7e9778968c6dbb55
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-05 22:58:04 +0800

    Add listing ui

--- front-end/src/app/features/pages/listing-page/listing-page.ts
+++ front-end/src/app/features/pages/listing-page/listing-page.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { PropertyService } from '../../../core/services/property-service/property-service';
+import { CardComponent } from '../../../shared/components/card-component/card-component';
 
 @Component({
   selector: 'app-listing-page',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './listing-page.html',
   styleUrl: './listing-page.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListingPage {}
+export class ListingPage implements OnInit {
+  private readonly propertyService = inject(PropertyService);
+
+  protected readonly properties = this.propertyService.allProperties;
+
+  ngOnInit(): void {
+    this.propertyService.getAllProperty();
+  }
+}
