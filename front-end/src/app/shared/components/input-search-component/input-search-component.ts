5e47b70e5e4303546fa558d726c3a9218945e872
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-06 23:58:14 +0800

    Add input-search-component

---
+++ front-end/src/app/shared/components/input-search-component/input-search-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-search-component',
+  imports: [],
+  templateUrl: './input-search-component.html',
+  styleUrl: './input-search-component.css',
+})
+export class InputSearchComponent {}
