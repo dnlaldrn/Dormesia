c36935db0956f0306016bf6b055a140be2f0496c
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-05-13 01:21:26 +0800

    Add property page ui

--- front-end/src/app/features/pages/property-details-page/property-details-page.ts
+++ front-end/src/app/features/pages/property-details-page/property-details-page.ts
@@ -1,14 +1,27 @@
 import { Component, inject,signal,computed, input } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { PropertyDetailsComponent } from '../../components/property-details-component/property-details-component';
+import { RouterLink } from '@angular/router';
+import { PropertyDetailsComponent } from '../../components/property-details-component/owner-details-component';
+import { PropertyService } from '../../../core/services/property-service/property-service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-property-details-page',
-  imports: [ RouterOutlet,PropertyDetailsComponent],
+  imports: [ RouterLink, PropertyDetailsComponent, CurrencyPipe],
   templateUrl: './property-details-page.html',
   styleUrl: './property-details-page.css',
 })
 export class PropertyDetailsPage {
+    private readonly propertyService = inject(PropertyService);
+  
+    // This input captures the 'propertyId' from the URL route automatically
+    // Note: URL params are strings, so we convert it to a number
+     readonly propertyId = input.required<string>();
+  
+    // Use computed to reactively find the property whenever the ID changes
+    readonly property = computed(() => {
+      const id = Number(this.propertyId());
+      return this.propertyService.getPropertyById(id);
+    });
    
   
 }
